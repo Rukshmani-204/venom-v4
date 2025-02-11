36046934f57ba73561ca5c2006f3e857b226cad7
Rukshmani-204 <rukshmanipatel2041980@gmail.com>
2025-02-11 11:17:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7784156456:AAExcre0xOEzyrLMgiqGaslD6DcgACRktWc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("mpatel926")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6551960654")
 
 
 
